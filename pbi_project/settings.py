a971080a71751b75fdcf9c13255c9bb1357a45a4
gazamcnulty <>
2023-08-12 13:39:32 +0000

    removed line from allowed hosts, getting gh heroku deployment error

--- pbi_project/settings.py
+++ pbi_project/settings.py
@@ -31,8 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["pbi_project.herokuapp.com",
-                 "8000-gazamcnulty-pp4-plant-ba-wo8nj9ioty.us2.codeanyapp.com"]
+ALLOWED_HOSTS = ["8000-gazamcnulty-pp4-plant-ba-wo8nj9ioty.us2.codeanyapp.com"]
 
 
 # Application definition
