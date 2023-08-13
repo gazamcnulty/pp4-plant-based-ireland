9fc4134877cd09454d13bb3faf4dd92b6c4b80d2
gazamcnulty <>
2023-08-13 15:05:28 +0000

    slight model changes, testing and working after migrations

--- pbi_app/models.py
+++ pbi_app/models.py
@@ -2,14 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
 # Create your models here.
 
 
 class Content(models.Model):
     title_of_post = models.CharField(max_length=150, unique=True, null=False)
-    photo = CloudinaryField()
-    extra_indo = models.TextField(null=True, blank=True)
-    slug = models.SlugField(unique=True)
+    photo = CloudinaryField('image', default='placeholder')
+    extra_info = models.TextField(null=True, blank=True)
+    slug = models.SlugField(unique=True, null=True)
     date_of_post = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
