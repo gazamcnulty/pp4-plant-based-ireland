892f0543bc9f3d8e0ae41546aa98a9b52de84d39
gazamcnulty <gazamcnulty@gmail.com>
2023-09-01 10:26:40 +0000

    changed staticfiles_storage settings

--- pbi_project/settings.py
+++ pbi_project/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATIC_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
