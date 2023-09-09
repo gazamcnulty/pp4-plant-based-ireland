c475dfd31b86ba76f085d13de7c05b8db37f45ec
gazamcnulty <gazamcnulty@gmail.com>
2023-09-09 11:23:09 +0000

    added email field to django usercreationform in forms.py, updated imports in forms.py and views.py

--- pbi_app/forms.py
+++ pbi_app/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.forms import EmailField 
+from django.utils.translation import ugettext_lazy as _ 
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from .models import Post , Gallery, News, Article # , Comment
 
 
@@ -6,6 +10,14 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['user','title', 'content', 'featured_image']
+
+
+class UserCreationForm(UserCreationForm):
+    email = EmailField(label=_("Email address"), required=True,
+        help_text=_("Required."))
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
 
 
 # class CommentForm(forms.ModelForm):
--- pbi_app/views.py
+++ pbi_app/views.py
@@ -4,13 +4,13 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.core.paginator import Paginator
 from django.views.generic import ListView
 from .models import Post, Comment, Gallery, News, Article
-from .forms import PostForm, GalleryForm, NewsForm  # CommentForm
+from .forms import PostForm, GalleryForm, NewsForm, UserCreationForm  # CommentForm
 
 
 # Create your views here.
